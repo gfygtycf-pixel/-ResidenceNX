7b08f505cbb7e1c051730a5414922d55567e34f5
gfygtycf-pixel <gfygtycf@gmail.com>
2026-07-05 20:55:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 package me.residencenx;
-package me.residencenx.listener;
 
 import cn.nukkit.plugin.PluginBase;
+
 import me.residencenx.manager.RegionManager;
 import me.residencenx.manager.SelectionManager;
+
 import me.residencenx.listener.PlayerListener;
+import me.residencenx.listener.BlockListener;
+
+import me.residencenx.command.RegionCommand;
 
 public class Main extends PluginBase {
 
@@ -30,14 +34,28 @@ public class Main extends PluginBase {
 
         instance = this;
 
+        // менеджеры
         this.selectionManager = new SelectionManager();
         this.regionManager = new RegionManager();
-        this.getServer().getCommandMap().register("rg", new me.residencenx.command.RegionCommand());
+
+        // конфиг
         saveDefaultConfig();
 
+        // слушатели
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         getServer().getPluginManager().registerEvents(new BlockListener(), this);
 
+        // команда /rg
+        this.getServer().getCommandMap().register(
+                "rg",
+                new RegionCommand()
+        );
+
         getLogger().info("ResidenceNX enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ResidenceNX disabled");
     }
 }
